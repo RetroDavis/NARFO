3c72dc3eb7b9717a245c2e20220db54b714f0117
MasulukeI <imasuluke@retrorabbit.co.za>
2019-01-29 09:38:49 +0200

    Axios call to Github API

--- Client/frontend/src/App.js
+++ Client/frontend/src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-
+import axios from 'axios'
 import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor () {
+    super()
+    this.state = {
+      username: 'will be changed with name from Github API'
+    }
+
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick () {
+    axios.get('https://api.github.com/users/maecapozzi')
+      .then(response => this.setState({username: response.data.name}))
+  }
+
+  render () {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Test api</button>
+        <p>{this.state.username}</p>
+      </div>
+    )
+}
 }
 
 export default App;
